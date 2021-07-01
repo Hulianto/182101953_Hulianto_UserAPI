c412b946cf325b1c49ee99ed5e5c6fbb71d65b25
Hulianto <hulianto9946@gmail.com>
2021-07-01 14:18:08 +0700

    Tugas User API pertemuan14 Hulianto(182101953)

---
+++ app/src/main/java/com/hulianto182101953/userprojectcrud/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hulianto182101953.userprojectcrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
